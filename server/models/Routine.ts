5705c1eac56aad74f0ed3293eb00758255bd4e63
njortiz <icholasnortiz@gmail.com>
2021-01-16 16:28:39 -0600

    add Routine model, plus Routine and Interval interfaces

---
+++ server/models/Routine.ts
@@ -0,0 +1,44 @@
+import { Schema, model, Document } from "mongoose";
+
+/*
+The Interval ranges over [startTime, endTime).
+All times are in seconds, based on the overall routine timeline.
+
+(e.g., startTime = 0 means it's the first interval. endTime = 120
+means this interval ends at 2 minutes and the next starts at 2 minutes)
+ */
+const IntervalSchema = new Schema({
+    name: String,
+    startTime: Number, 
+    endTime: Number, 
+});
+
+const RoutineSchema = new Schema({
+  name: String,
+  duration: Number, 
+  intervals: [IntervalSchema],
+  isPublic: Boolean,
+  creator_id: String, // _id of the User who originally created the routine
+  owner_id: String, // _id of the User that has this routine saved
+});
+
+export interface Interval extends Document {
+  name: string;
+  startTime: number;
+  endTime: number;
+  _id: string;
+}
+
+export interface Routine extends Document {
+    name: string;
+    duration: number;
+    intervals: Interval[];
+    isPublic: boolean;
+    creator_id: string;
+    owner_id: string;
+    _id: string;
+}
+
+const RoutineModel = model<Routine>("Routine", RoutineSchema);
+
+export default RoutineModel;
