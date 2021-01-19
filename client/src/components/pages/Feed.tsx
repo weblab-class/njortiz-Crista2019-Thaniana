ed5a8cf9a9db2698bdc608abd1a45f761ee51a2f
Crista2019 <crista2019@gmail.com>
2021-01-18 16:07:58 -0800

    "centering routines list on feed"

--- client/src/components/pages/Feed.tsx
+++ client/src/components/pages/Feed.tsx
@@ -1,5 +1,5 @@
 import NavBar from "../modules/NavBar";
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { RouteComponentProps, navigate } from "@reach/router";
 import "./Feed.css";
 import SingleRoutine from "../modules/SingleRoutine.tsx";
@@ -9,10 +9,10 @@ import { Routine } from "../modules/SingleRoutine";
 import { get } from "../../utilities";
 
 import GoogleLogin, {
-    GoogleLoginResponse,
-    GoogleLoginResponseOffline,
-    GoogleLogout,
-  } from "react-google-login";
+  GoogleLoginResponse,
+  GoogleLoginResponseOffline,
+  GoogleLogout,
+} from "react-google-login";
 // import Routine from "./Routine";
 
 import User from "../../../../shared/User";
@@ -23,74 +23,71 @@ type Props = {
   handleLogin: (res: GoogleLoginResponse | GoogleLoginResponseOffline) => void;
   handleLogout: () => void;
 };
-//does the below set the state, not sure if this is how it is done in typescript 
+//does the below set the state, not sure if this is how it is done in typescript
 type State = {
   loggedIn: boolean;
-  routines: Routine[] ;
+  routines: Routine[];
 };
 
-
-
 class Feed extends Component<Props & RouteComponentProps, State> {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       loggedIn: false,
-      routines: [] ,
-    }
+      routines: [],
+    };
   }
   componentDidMount() {
     get("/api/public-routines").then((routineObjs: Routine[]) => {
       this.setState({
-        routines:routineObjs,
+        routines: routineObjs,
       });
     });
   }
 
-    render() {
-      if (!this.props.user) {
-        navigate(-1);
-        return null;
-      }
-      let routinesList = null;
-      const hasStories = this.state.routines.length !== 0;
-      if (hasStories) {
-        routinesList = this.state.routines.map((routinesObj) => (
-          <SingleRoutine
-            _id={routinesObj._id}
-            name={routinesObj.name}
-            creator_id={routinesObj.creator_id}
-            content={routinesObj.content}//actually the routine name which I do not see in the database as of now 
-          />
-        ));
-      } else {
-        routinesList = <div>No stories!</div>;
-      }
+  render() {
+    if (!this.props.user) {
+      navigate(-1);
+      return null;
+    }
+    let routinesList = null;
+    const hasStories = this.state.routines.length !== 0;
+    if (hasStories) {
+      routinesList = this.state.routines.map((routinesObj) => (
+        <SingleRoutine
+          _id={routinesObj._id}
+          name={routinesObj.name}
+          creator_id={routinesObj.creator_id}
+          content={routinesObj.content} //actually the routine name which I do not see in the database as of now
+        />
+      ));
+    } else {
+      routinesList = <div>No stories!</div>;
+    }
 
-
-
-      return (
-        
-        <>
-          <NavBar
+    return (
+      <>
+        <NavBar
           handleLogin={this.props.handleLogin}
           handleLogout={this.props.handleLogout}
           user={this.props.user}
-          />
-        <div>
-          {routinesList}
-          {console.log(JSON.stringify(this.state.routines))}
+        />
+        <div className="container">
+          <div>
+            {routinesList}
+            {console.log(JSON.stringify(this.state.routines))}
 
-{/*           
+            {/*           
           <SingleRoutine 
           _id ="123" 
           creator_name ="batman" 
           creator_id="567" 
           content="hi I am fun"/> */}
+          </div>
         </div>
-        </>
-      )
-    }
+      </>
+    );
   }
-  
-  export default Feed;
+}
+
+export default Feed;
